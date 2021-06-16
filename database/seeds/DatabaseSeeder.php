bd6cde1eca9ed25c442b867d18056cede20e7994
izanagiwarrior <m.faiztpaduhai@gmail.com>
2021-06-16 18:09:58 +0700

    seeder fixed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Admin;
+use App\Customer;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +12,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $customer = [
+            [
+                'username' => 'Example User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('123456'),
+                'notelp' => "081234567890",
+                'alamat' => "bandung",
+            ],
+            [
+                'username' => 'Example User2',
+                'email' => 'user2@gmail.com',
+                'password' => bcrypt('123456'),
+                'notelp' => "081234567891",
+                'alamat' => "bandung",
+            ],
+        ];
+
+        foreach ($customer as $key => $value) {
+            Customer::create($value);
+        }
+
+        $admin = [
+            [
+                'username' => 'admin',
+                'password' => bcrypt('123456'),
+            ],
+        ];
+
+        foreach ($admin as $key => $value) {
+            Admin::create($value);
+        }
     }
 }
